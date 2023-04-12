be31f8c1a10065b06a51cca9ff04a8243c8678a4
jmejia1920 <jonny86493@gmail.com>
2023-04-12 10:04:21 -0400

    Added new files

---
+++ MyCodingSkills.java
@@ -0,0 +1,18 @@
+public class MyCodingSkills {
+    public static void main(String args[]) {
+        System.out.println("Java");
+        System.out.println("C++");
+        System.out.println("Python");
+        System.out.println("SQL");
+        System.out.println("Javascript");
+        System.out.println("Rust");
+        System.out.println("Ruby");
+        System.out.println("AWS");
+
+
+
+    }
+}
+
+
+
